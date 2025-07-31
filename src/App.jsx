2af259565373822ed5b38942f29c4b8173ae6eee
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-31 04:58:13 +0200

    Add Form component to App for user input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ToastContainer } from "react-toastify";
 import Posts from "./components/Posts";
+import Form from "./components/Form";
 function App() {
   return (
     <>
       <ToastContainer position="bottom-left" />
       <Posts />
+      <Form />
     </>
   );
 }
