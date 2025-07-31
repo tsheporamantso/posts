08acf204ee5469701b04672836d20ab5273ffb1d
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-31 06:39:22 +0200

    Implement delete functionality in SinglePost component with error handling

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,19 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React from "react";
+import customFetch from "../axios/utils";
+import { toast } from "react-toastify";
 
-const SinglePost = ({ title, body }) => {
-  const basicStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    gap: "1rem",
-  };
+const SinglePost = ({ id, title, body }) => {
+  const queryClient = useQueryClient();
+  const { mutate: deletePost } = useMutation({
+    mutationFn: (postId) => customFetch(`/posts/${postId}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      toast.success("Post Deleted Successfully");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
 
   const basicStyles = { textTransform: "capitalize", textAlign: "center" };
 
   return (
-    <div style={basicStyle}>
+    <div>
       <article>
         <h3 style={basicStyles}>{title}</h3>
         <p style={basicStyles}>{body}</p>
+        <button
+          style={{
+            textAlign: "center",
+            width: "100%",
+          }}
+          onClick={() => deletePost(id)}
+        >
+          Delete Post
+        </button>
       </article>
     </div>
   );
