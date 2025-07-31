99ccb0edafe8e99ce5a90f95d586489dee0f53de
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-31 05:28:37 +0200

    Refactor Form component to handle title and body state separately and improve form structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,33 +2,41 @@ import { useState } from "react";
 import { useCreatePost } from "../hooks/reactQueryCustomHooks";
 
 const Form = () => {
-  const [text, setText] = useState("");
-  const { createPost, isPending } = useCreatePost(setText);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const { createPost, isPending } = useCreatePost(setTitle, setBody);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createPost(text);
+    if (!title || !body) return;
+    createPost(title, body);
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
-    >
-      <label htmlFor="title" style={{ textAlign: "center" }}>
-        Create Post
-      </label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button type="submit" disabled={isPending} style={{}}>
-        Create Post
-      </button>
-    </form>
+    <>
+      <h2 style={{ textAlign: "center" }}>Create Post</h2>
+      <form onSubmit={handleSubmit} style={{ display: "flex", gap: "1rem" }}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="Body">Body:</label>
+        <input
+          type="body"
+          name="body"
+          id="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button type="submit" disabled={isPending} style={{}}>
+          Create Post
+        </button>
+      </form>
+    </>
   );
 };
 
